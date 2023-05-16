ab167ec4cba0aec0253f186e1b314b37589a84cd
gedaias jair bg <gedaias@geda.dev>
2023-05-15 21:06:37 -0600

    Create the buildBalancedTree method
    Validate if the array is empty, and return null
    Sort the array and calculate the middle index
    Create a new node with the value at the middle index of the array
    Create the left and right array by slicing the original and leaving out the current mid node
    Asign the value returned from the recursive call to the leftChild and rightChild properties of the node
    Return the node

--- bst.js
+++ bst.js
@@ -4,4 +4,21 @@ export default class BinarySearchTree {
   constructor(arr) {
     this.root = this.buildBalancedTree(arr);
   }
+
+  buildBalancedTree(arr) {
+    if (arr.length === 0) {
+      return null;
+    }
+    const sortedArr = arr.sort((a, b) => a - b);
+    const midIndex = Math.floor(sortedArr.length / 2);
+    const node = new Node(sortedArr[midIndex]);
+
+    const leftArr = sortedArr.slice(0, midIndex);
+    const rightArr = sortedArr.slice(midIndex + 1);
+
+    node.leftChild = this.buildBalancedTree(leftArr);
+    node.rightChild = this.buildBalancedTree(rightArr);
+
+    return node;
+  }
 }
